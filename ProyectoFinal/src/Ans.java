e1d648922dc8ef00b0f00d6d9a8b72886513203e
s-choieve <s-choieve@53S50030615.it.bsd405.org>
2017-06-13 09:57:21 -0700

    Changed the Ans constructor, and made the Chatbot capable of opening objects.txt

--- ProyectoFinal/src/Ans.java
+++ ProyectoFinal/src/Ans.java
@@ -3,9 +3,11 @@ public class Ans {
 	private String tag;
 	private String name;
 	
-	public Ans(String name, String tag) {
-		this.tag = tag;
-		this.name = name;
+	
+	public Ans(String input) {
+		//This is just an overloaded constructor, identical to the first one
+		this.tag = input.split(":")[1];
+		this.name = input.split(":")[0];
 	}
 	
 	public String[] getTag() {
--- ProyectoFinal/src/Chatbot.java
+++ ProyectoFinal/src/Chatbot.java
@@ -1,9 +1,31 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Chatbot {
 	public static void main(String[] args) {
-	ArrayList<Ans> objects = new ArrayList<Ans>();
-	ArrayList<String> tags = new ArrayList<String>();
-	
+		System.out.println("Hola!  Yo soy un proyecto de Grant y Everett");
+		System.out.println("Please think of either something that has to do with disasters or food.");
+		System.out.println("I will ask you questions, please answer honestly.");
+		Scanner input = new Scanner(System.in);
+		File f = new File("objects.txt");
+		Scanner file;
+		//Chatbot bot = new 
+		if(f.exists()){
+			file = new Scanner("objects.txt");
+			ArrayList<Ans> objects = new ArrayList<Ans>();
+			ArrayList<String> tags = new ArrayList<String>();
+			while(file.hasNextLine()){
+				objects.add(new Ans(file.nextLine()));
+			}
+		}
+		else{
+			System.out.println("Error!  File not found!");
+		}
+		
+		
+		
+		
+	}
 	
 }
