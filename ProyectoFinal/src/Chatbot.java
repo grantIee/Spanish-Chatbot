89b6d4bf89c1af711014fd5cb6da840b7cccae06
s-choieve <s-choieve@53S50030615.it.bsd405.org>
2017-06-13 10:06:30 -0700

    Added the Qes class to hold questions and their tags

--- ProyectoFinal/src/Chatbot.java
+++ ProyectoFinal/src/Chatbot.java
@@ -8,13 +8,16 @@ public class Chatbot {
 		System.out.println("Please think of either something that has to do with disasters or food.");
 		System.out.println("I will ask you questions, please answer honestly.");
 		Scanner input = new Scanner(System.in);
+		Scanner getQ = new Scanner(System.in);
 		File f = new File("objects.txt");
+		File q = new File("")
 		Scanner file;
 		//Chatbot bot = new 
 		if(f.exists()){
 			file = new Scanner("objects.txt");
 			ArrayList<Ans> objects = new ArrayList<Ans>();
 			ArrayList<String> tags = new ArrayList<String>();
+			ArrayList<String> questions = new ArrayList<String>();
 			while(file.hasNextLine()){
 				objects.add(new Ans(file.nextLine()));
 			}
